44d51d9aa2c5d7b04b98daae0390e8ce6fbcf977
Pavalean <etbag@yahoo.com>
2022-07-13 19:33:31 +0300

    prima versiune

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // instructiune 1: sa se afiseze "Hello World"
+        System.out.println("Hello World!\nCe faci?\tTotul bine?");
+       // System.out.println("Salutare!\nEsti bine?");
+        /* Comentariu
+        pe mai
+        multe
+        linii.
+         */
+//        CTRL + / = comenteaza liniile selectate
+//                cu comment simplu
+
+      /*CTRL + SHIFT + / = comenteaza liniile selectate
+                cu "Comment ALl"*/
+
+    }
+}
---
+++ src/TipuriDeDate.java
@@ -0,0 +1,33 @@
+public class TipuriDeDate {
+    //valori default pentru primitive daca sunt definite la nivel de clasa.
+    static boolean anaAreMere1;
+    public static void main(String[] args) {
+        //PRIMITIVE
+        boolean anaAreMere = true;
+        byte numarMere = 127;
+
+        numarMere = 12;
+        System.out.println("Are ana mere?: " + anaAreMere);
+        System.out.println("Numar mere: " + numarMere);
+
+        char caracterulA = 'A';
+        System.out.println("Caracterul retinut de variabila este: " + caracterulA);
+
+
+        int numnarMare = 300_000;
+        long numarFoarteMare = 300_000_000_000L;
+        char a = 98;
+        System.out.println("Caracter: " + a);
+
+
+        //CURS 2
+        byte b;
+        b = 12;
+        System.out.println("Byte:\t" + b);
+        System.out.println("Long: "+ numarFoarteMare);
+
+
+
+
+    }
+}
